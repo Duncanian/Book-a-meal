f842205abc3345ad15701bcaed529e49d8f572f0
Duncanian <ianduncan08@gmail.com>
2018-04-24 17:29:16 +0300

    [chore #156810722] Write tests on the end points

---
+++ Api/user.py
@@ -0,0 +1,19 @@
+class User(object):
+	"""docstring for Caterer"""
+	def __init__(self):
+		pass
+
+	def login(self):
+		pass
+
+	def signup(self):
+		pass
+
+	def get_menu(self):
+		pass
+
+	def make_order(self):
+		pass
+
+	def modify_order(self):
+		pass
